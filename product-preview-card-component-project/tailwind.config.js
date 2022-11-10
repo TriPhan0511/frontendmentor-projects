c23b8f017fdc6ee1d437e8822874ee7c6c7a361c
Tri Phan <tripp@ames.edu.vn>
2022-11-10 12:00:50 +0700

    Updated

--- product-preview-card-component-project/tailwind.config.js
+++ product-preview-card-component-project/tailwind.config.js
@@ -8,8 +8,9 @@ module.exports = {
 		extend: {
 			colors: {
 				darkCyan: 'hsl(158, 36%, 37%)',
+				darkCyanActiveState: 'hsl(157, 46%, 17%)',
 				cream: 'hsl(30, 38%, 92%)',
-				veryDarkBlue: 'hsl(30, 38%, 92%)',
+				veryDarkBlue: 'hsl(212, 21%, 14%)',
 				darkGrayishBlue: 'hsl(228, 12%, 48%)',
 			},
 			fontFamily: {
