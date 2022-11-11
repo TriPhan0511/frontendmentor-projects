ef5c0bf5653f4af5f999ba951c9e4debf3653f6b
Tri Phan <tripp@ames.edu.vn>
2022-11-11 16:18:22 +0700

    Updated

--- nft-preview-card-component-project/tailwind.config.js
+++ nft-preview-card-component-project/tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{html,js}'],
 	theme: {
 		extend: {
 			colors: {
 				softBlue: 'hsl(215, 51%, 70%)',
-				myCyan: 'hsl(178, 100%, 50%)',
+				cyan: 'hsl(178, 100%, 50%)',
+				veryDarkBlueMainBg: 'hsl(217, 54%, 11%)',
+				veryDarkBlueCardBg: 'hsl(216, 50%, 16%)',
+				veryDarkBlueLine: 'hsl(215, 32%, 27%)',
 			},
+			fontFamily: { outfit: ['Outfit', ...defaultTheme.fontFamily.sans] },
 		},
 	},
 	plugins: [],
